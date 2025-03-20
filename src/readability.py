f83bd806e7f36f4f9204ed552f6e9f9dfd92b2e8
Ethan Fang <theethanfang@gmail.com>
2025-03-19 20:10:44 -0400

    added readability.py, sentiment.py for Charan and Karel

---
+++ src/readability.py
@@ -0,0 +1,8 @@
+# Readability features from Chin et. al.
+
+def coleman_liau():
+    return 0
+
+def dale_chall():
+    return 0
+
---
+++ src/sentiment.py
@@ -0,0 +1,8 @@
+# Sentiment features from Chin et. al.
+
+def LM_positive():
+    return 0
+
+def LM_negative():
+    return 0
+
